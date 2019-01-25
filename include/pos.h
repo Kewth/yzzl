2ffdaa8e649b13d2f37334f66693eb36f9c4b6d2
Kewth <Kewth.K.D@outlook.com>
2019-01-25 19:40:34 +0800

    添加 coord_map 的 pos 查询方法

--- include/pos.h
+++ include/pos.h
@@ -17,10 +17,12 @@ namespace pos {
 	public:
 		int high, width;
 		/* T& operator () (int, int); */
+		/* T& operator () (Pos); */
 		/* Coord_map<T>(int, int); */
 		// 我 tm 是真的 tm 的对模板类无语，贴声明连编译都过不了
 		// 到时候再改，先用着
 		T& operator () (int x, int y) { return _map[pos::Pos(x, y)]; }
+		T& operator () (Pos p) { return _map[p]; }
 		Coord_map<T>(int x, int y): high(x), width(y) {}
 	};
 };
