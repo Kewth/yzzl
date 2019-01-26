15b2ca953f9583ac5a3fdf85aaaac5cb33e38cbd
Kewth <Kewth.K.D@outlook.com>
2019-01-26 16:45:54 +0800

    修改 lib/pos.cpp 格式

--- lib/pos.cpp
+++ lib/pos.cpp
@@ -1,31 +1,30 @@
 #include "../include/pos.h"
 
-using namespace pos;
+namespace pos {
+	pos::Pos::Pos(int x, int y): _x(x), _y(y) {
+	}
+	bool pos::Pos::operator < (pos::Pos an) const {
+		return _x == an._x ? _y < an._y : _x < an._x;
+	}
 
-pos::Pos::Pos(int x, int y): _x(x), _y(y) {
-}
+	// 我 tm 是真的 tm 的对模板类无语，贴声明连编译都过不了
+	// 到时候再改，先用着
+	/* template<class T> */
+	/* T& pos::Coord_map<T>::operator () (int x, int y) { */
+	/* 	return _map[pos::Pos(x, y)]; */
+	/* } */
 
-bool pos::Pos::operator < (pos::Pos an) const {
-	return _x == an._x ? _y < an._y : _x < an._x;
-}
+	/* template<class T> */
+	/* pos::Coord_map<T>::Coord_map(int high, int width): */
+	/* 	high(high), width(width) { */
+	/* } */
 
-// 我 tm 是真的 tm 的对模板类无语，贴声明连编译都过不了
-// 到时候再改，先用着
-/* template<class T> */
-/* T& pos::Coord_map<T>::operator () (int x, int y) { */
-/* 	return _map[pos::Pos(x, y)]; */
-/* } */
-
-/* template<class T> */
-/* pos::Coord_map<T>::Coord_map(int high, int width): */
-/* 	high(high), width(width) { */
-/* } */
-
-/* int main() { */
-/* 	std::map<pos::Pos, int> mp; */
-/* 	pos::Pos p(1, 2), pp(2, 1); */
-/* 	if(p < pp) */
-/* 		return  0; */
-/* 	pos::Coord_map<char> map(1, 2); */
-/* 	map(0, 0) = '1'; */
-/* } */
+	/* int main() { */
+	/* 	std::map<pos::Pos, int> mp; */
+	/* 	pos::Pos p(1, 2), pp(2, 1); */
+	/* 	if(p < pp) */
+	/* 		return  0; */
+	/* 	pos::Coord_map<char> map(1, 2); */
+	/* 	map(0, 0) = '1'; */
+	/* } */
+};
